34c65d890a357d29c037942caa44c3d2df38f1c4
Karan Singh <karans9609@gmail.com>
2023-12-20 23:40:09 +0530

    Playsound Module

--- 03_playsound_example.py
+++ 03_playsound_example.py
@@ -1,5 +1,7 @@
 # playsound helps to play sound(.mp3 files)
 
+# pip install playsound write this in terminal to install playsound module using pip
+
 from playsound import playsound
 playsound("E:\\Whatsapp\\Mobile\\WhatsApp Audio\\Received\\AUD-20210406-WA0003.mp3")
 
