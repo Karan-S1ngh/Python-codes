ba15fa2e4189a851ac5935395687f5c16c1b08a8
Karan Singh <karans9609@gmail.com>
2023-12-20 23:26:52 +0530

    5 Marks Display Program

--- 09_display_marks_program.py
+++ 09_display_marks_program.py
@@ -11,3 +11,14 @@ marks_list = [m1,m2,m3,m4,m5]
 marks_list.sort()
 
 print(marks_list)
+
+
+
+'''OUTPUT
+Enter marks 1 : 45
+Enter marks 2 : 78
+Enter marks 3 : 23
+Enter marks 4 : 56
+Enter marks 5 : 89
+[23, 45, 56, 78, 89]
+'''
