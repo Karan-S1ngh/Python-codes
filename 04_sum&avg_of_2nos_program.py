ba13299a87e7c631bcbdf4e977e988ab963fa5d7
Karan Singh <karans9609@gmail.com>
2023-12-20 23:21:08 +0530

    Sum and Avg of 2 numbers

--- 04_sum&avg_of_2nos_program.py
+++ 04_sum&avg_of_2nos_program.py
@@ -5,3 +5,18 @@ b = int(input("Enter second number : "))
 
 print("Sum of both the numbers is :", a+b )
 print("Average of two number is :", (a+b)/2)
+
+
+
+
+'''OUTPUT
+Enter first number : 5
+Enter second number : 6
+Sum of both the numbers is : 11
+Average of two number is : 5.5
+
+Enter first number : 10
+Enter second number : 20.5
+Sum of both the numbers is : 30.5
+Average of two number is : 15.25
+'''
