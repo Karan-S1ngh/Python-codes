41b1b2b81364d52ca9f7fdea3cee64a7c485a218
Karan Singh <karans9609@gmail.com>
2023-12-30 21:56:08 +0530

    Updated

--- Informations/02_os_example.py
+++ Informations/02_os_example.py
@@ -1,6 +1,14 @@
 # 'pip install os' write this in terminal to install os module using pip
 
+
 import os
 print(os.listdir())
-
 # Lists all the file in this directory
+
+
+os.chdir("Files")
+# changes the directory to the mentioned folder (here, Files)
+
+
+os.mkdir("Test")
+# makes a new folder in the directory(here, Test)
--- Programs/18_folder_of_multiplication_tables.py
+++ Programs/18_folder_of_multiplication_tables.py
@@ -1,5 +1,6 @@
 # WAP to make a folder and place multiple files, each file having a table of one number each
 
+
 a = int(input("Enter the first number from where you want multiplication table : "))
 
 b = int(input("Enter the last number till where you want multiplication table : "))
@@ -7,12 +8,28 @@ b = int(input("Enter the last number till where you want multiplication table : 
 c = int(input("Enter the number till which u want each number's table : "))
 
 
-for i in range(a, b):
+import os
+os.chdir("Files")
+# changes the directory to Files
+os.mkdir("Tables")
+# makes a table folder in the directory
+
+
+for i in range(a, b+1):
     with open(f"Tables/Multiplication table of {i}",'w') as f:
         for j in range(1, c+1):
             f.write(f"{i} x {j} = {i*j}\n")
+# creating files for each multiplication table
             
 
 print("Folder generated")
-            
 
+
+
+
+'''OUTPUT
+Enter the first number from where you want multiplication table : 2
+Enter the last number till where you want multiplication table : 20
+Enter the number till which u want each number's table : 10
+Folder generated
+'''
