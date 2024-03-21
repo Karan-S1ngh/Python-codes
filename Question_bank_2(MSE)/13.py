144fafd3dc47098de4be8827a734538b734e5f30
Karan Singh <karans9609@gmail.com>
2024-03-21 10:13:41 +0530

    Updated

--- Question_bank_2(MSE)/13.py
+++ Question_bank_2(MSE)/13.py
@@ -1,27 +1,53 @@
 # Calculate sum and average of strings and display it
 
 
-# Input a string
-input_string = input("Enter a string: ")
+n = int(input("Enter no of strings: "))
 
-# Split the string into words and calculate sum and average
-words = input_string.split()
-total_length = sum(map(len, words))
-average_length = total_length / len(input_string) if total_length > 0 else 0
+list = []
+for i in range (1,n+1):
+    input_string = input(f"Enter string {i} : ") 
+    int_string = int(input_string)
+    list.append(int_string)
+    
+sum_string = sum(list)
+print(sum_string)
 
-# Display results
-print("Sum of lengths of all words:", total_length)
-print("Average length of words:", average_length)
+avg_string = sum(list)/len(list)
+if len(list) > 0:
+    print(avg_string)
+else:
+    print(0)
 
 
 
 
 '''OUTPUT
-Enter a string: hiii
-Sum of lengths of all words: 4
-Average length of words: 1.0
+Enter no of strings: 0
+0
+0
 
-Enter a string: Hello how are u
-Sum of lengths of all words: 12
-Average length of words: 0.8
-'''
+Enter no of strings: 3
+Enter string 1 : 5
+Enter string 2 : 10
+Enter string 3 : 15
+30
+10.0
+
+Enter no of strings: 5
+Enter string 1 : 1
+Enter string 2 : 2
+Enter string 3 : 3
+Enter string 4 : 4
+Enter string 5 : 5
+15
+3.0
+
+Enter no of strings: 5
+Enter string 1 : 0
+Enter string 2 : 0
+Enter string 3 : 0
+Enter string 4 : 0
+Enter string 5 : 0
+0
+0.0
+''' 
